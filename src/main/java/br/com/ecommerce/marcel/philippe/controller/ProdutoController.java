b33e1896e1b3b18fdad0a4657565ef41134453fe
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2024-11-02 21:05:17 -0300

    Inserção da tag para o mapeamento do swagger.

--- src/main/java/br/com/ecommerce/marcel/philippe/controller/ProdutoController.java
+++ src/main/java/br/com/ecommerce/marcel/philippe/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +19,12 @@ import br.com.ecommerce.marcel.philippe.service.ProdutoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
+@Tag(name = "Produtos", description = "Gerenciamento de produtos")
 public class ProdutoController {
 
 	@Autowired
